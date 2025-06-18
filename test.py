289120680f6e0eb5174294793451aadb6e97a9f2
Fuma <leonardfuma26@gmail.com>
2025-06-18 23:29:16 +0200

    Python Basics

---
+++ test.py
@@ -0,0 +1,52 @@
+# setting up
+
+# print("Hello World")
+
+# strings part 01
+
+# case 1
+# message = 'Hello World'
+
+# print(message)
+
+# case 2
+# message = "Stash's World"
+
+# print(message)
+
+# case 3
+# message = """Stash's 
+# World"""
+
+# print(message)
+
+# case 4
+# message = "Hello World!"
+
+# print(message.upper())
+
+# case 5
+# message = "Hello, World!"
+
+# print(message.split(','))
+
+# case 5
+# message = "Hello World!"
+
+# print(message.lower())
+
+# case 6
+# message = " Hello World! "
+
+# print(message.strip())
+
+# case 7
+# message = "Hello World!"
+
+# print(message[0])
+
+# case 8
+# message = "Hello World!"
+# new_message = message.replace("World!", "People")
+
+# print(new_message)
