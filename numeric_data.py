82df8088b9957ded279c4ff6eb85d82dfb0bbdb2
Fuma <leonardfuma26@gmail.com>
2025-06-20 22:49:23 +0200

    Operators on numeric data

--- numeric_data.py
+++ numeric_data.py
@@ -1,11 +1,31 @@
 # case 1
-num = 3
+# num = 3
 
-print(num)
+# print(num)
 
 # case 2
-num = 6
-num2 = 3.14
+# num = 6
+# num2 = 3.14
 
-print(type(num))
-print(type(num2))
+# print(type(num))
+# print(type(num2))
+
+# case 3
+
+# x = 8
+# y = 4
+
+# print(x+y)  #12
+# print(x-y)  #4
+# print(x*y)  #32
+# print(x/y)  #2.0
+# print(x%y)  #0
+# print(x**y)  #4096
+
+
+# case 4
+
+x = 10
+x **= 2
+
+print(x)
