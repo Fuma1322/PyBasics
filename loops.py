ad4148f13f85422a08927585ecad7ab508ba578a
Fuma <leonardfuma26@gmail.com>
2025-06-21 00:03:58 +0200

    Loops

---
+++ loops.py
@@ -0,0 +1,20 @@
+# Loops
+
+# for loop
+fruits = ["apple", "banana", "orange"]
+
+for fruit in fruits:
+    print(fruit)
+    
+numbers = [1,2,3,4]
+
+for number in numbers:
+    print(number)
+    
+# while loop
+
+num = 1
+
+while num <= 5:
+    print(num)
+    num += 1 #Inrements the num by 1
